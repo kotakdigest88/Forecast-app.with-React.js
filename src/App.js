ad5e576e7b0ee1bc2ccc2b72c28ef113e0811910
nsalina <nsalina76@gmail.com>
2024-05-23 08:07:53 +0800

    fix bug on footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ function App() {
   <div className="App">
     <div className="container">
      <Weather defaultcity="Melaka" />
-     <footer>Coded by <a href="https://github.com/kotakdigest88">nsalina</a> and it is <a href="https://github.com/kotakdigest88/shecodereact">open-sources</a> on gitHub</footer>
+     <footer>Coded by {""}
+       <a href="https://github.com/kotakdigest88" target="_blank" rel="noreferrer">nsalina</a>{""}
+     and it is{""}
+      <a href="https://github.com/kotakdigest88/shecodereact" target="_blank" rel="noreferrer">
+      {""}  open-sources
+        </a> 
+      {""}  on gitHub</footer>
   </div> 
   </div>
   );
